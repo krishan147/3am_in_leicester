7a74b4a4e91994f1ec440925d4a772af0b61873b
Krishan Vadher <krishan.vadher@gmail.com>
2023-06-10 20:26:22 +0100

    save system

---
+++ global_options/global_options.gd
@@ -0,0 +1,32 @@
+extends Node
+
+var file_location = OS.get_user_data_dir() + "/save.save"
+
+func _ready():
+	if FileAccess.file_exists(file_location):
+		pass
+	else:
+		FileAccess.open(file_location, FileAccess.WRITE_READ)
+		
+func _saveGame(data):
+	var file = FileAccess.open(file_location, FileAccess.WRITE)
+	file.store_line(JSON.stringify(data))
+	file.close()
+
+func _loadGame():
+	var file = FileAccess.open(file_location, FileAccess.READ)
+	var data = JSON.parse_string(file.get_as_text())
+	file.close()
+	return data
+
+func _saveGameTemplate():
+	var save_dict = {
+		"player_location": 1,
+		"red_sauce": false,
+		"bacon": false,
+		"baps":false,
+		"blue_crisps":false, 
+		"green_crisps":false,
+		"red_crisps":false
+		}
+	return save_dict
--- menu/menu.gd
+++ menu/menu.gd
@@ -1,16 +1,12 @@
 extends Node3D
 @onready var options = $menu_buttons/SubViewport/options
+@onready var vol_num = 50
+@onready var camera_menu = $menu_Camera3D
+@onready var camera_player = $"../player_main/SpringArmPivot/SpringArm3D/Camera3D"
+@onready var menu_buttons = $menu_buttons
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-
 
 func _on_options_pressed():
 	options.visible = true
@@ -24,13 +20,36 @@ func _on_exit_pressed():
 	get_tree().quit()
 
 
-func _on_volume_h_slider_value_changed(value):
-	print (value)
+func _on_volume_h_slider_value_changed(value): # all
+	vol_num = _volumeLookup(value)
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), vol_num)
 
 
-func _on_volume_h_slider_2_value_changed(value):
-	print (value)
+func _on_volume_h_slider_2_value_changed(value): # music
+	vol_num = _volumeLookup(value)
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("music"), vol_num)
+
+func _volumeLookup(dial_num):
+	
+	var dict_volume = {
+		100:6,
+		90:-1,
+		80:-8,
+		70:-15,
+		60:-22,
+		50:-29,
+		40:-44,
+		30:-51,
+		20:-65,
+		10:-79,
+		0:-80,
+	}
+	
+	return dict_volume[int(dial_num)]
 
 
-func _on_brightness_h_slider_value_changed(value):
-	print (value)
+func _on_new_game_button_down():
+	menu_buttons.visible = false
+	self.visible = false
+	camera_menu.current = false
+	camera_player.current = true
